82f34c33b405034323658321f1d6d4998b10c8d6
Adrien Lina <adrienlina@gmail.com>
2017-06-04 22:32:23 +0200

    Add InfoWindow to markers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react';
-import Map, { GoogleApiWrapper} from 'google-maps-react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Map, { GoogleApiWrapper, InfoWindow } from 'google-maps-react';
 import Marker from './imports/Marker.js';
 
 export class App extends React.Component {
@@ -7,21 +8,61 @@ export class App extends React.Component {
     super(props, context);
 
     this.state = {
-      places: [],
-      pagination: null
+      showingInfoWindow: false,
+      activeMarker: {},
     };
+
+    this.onMapClicked = this.onMapClicked.bind(this);
+    this.onMarkerClick = this.onMarkerClick.bind(this);
   }
+
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked(props) {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+
   render() {
+    const markers = [{
+      name: 'Sicara',
+      title: 'Sicara',
+      position: {lat: 48.8828993, lng: 2.320023},
+    }];
+
     return (
-        <Map
-          google={this.props.google}
-          center={{lat: 48.8828993, lng: 2.320023}}
-        >
-          <Marker
-            title={'Sicara'}
-            name={'Sicara'}
-            position={{lat: 48.8828993, lng: 2.320023}} />
-        </Map>
+      <Map
+        google={this.props.google}
+        center={markers[0].position}
+      >
+        {markers.map(marker => {
+          return (
+            <Marker
+              key={marker.title}
+              title={marker.title}
+              name={marker.name}
+              position={marker.position}
+              onClick={this.onMarkerClick}
+            />
+          );
+        })}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.activeMarker.title}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
@@ -31,5 +72,5 @@ App.propTypes = {
 };
 
 export default GoogleApiWrapper({
-  apiKey: 'theAPIkey',
+  apiKey: 'yourToken',
 })(App);
